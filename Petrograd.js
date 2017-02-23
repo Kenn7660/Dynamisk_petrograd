9c12ad80cdeb5dd3db4f949300d9861a94f96959
KennethNielsen89 <kolob771@hotmail.com>
2017-02-23 13:15:31 +0100

    Opdate nyeste version

--- Petrograd.js
+++ Petrograd.js
@@ -4,7 +4,6 @@ function sidenVises() {
     console.log("siden vises");
 
     $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
-    visProdukt();
 }
 
 function visProduktListe( listen ) {
@@ -14,6 +13,19 @@ function visProduktListe( listen ) {
 
 function visProdukt( produkt ) {
     var klon = document.querySelector(".produkt_template").content.cloneNode(true);
+
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris+" DKK";
+
+    var rabatpris = Math.ceil(produkt.pris - (produkt.pris*produkt.rabatsats/100));
+    klon.querySelector(".data_rabat").innerHTML = rabatpris;
+
+    klon.querySelector(".data_billede").src = "/imgs/small/" + produkt.billede + "-sm.jpg";
+
+    if(produkt.udsolgt == false) {
+        var udsolgttekst = klon.querySelector(".udsolgt");
+        udsolgttekst.parentNode.removeChild( udsolgttekst );
+    }
 
     document.querySelector(".produkt_liste").appendChild(klon);
 }
