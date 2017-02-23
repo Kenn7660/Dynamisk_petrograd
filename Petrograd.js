5dc8d5d350f5130ac9c16706404486faf4c41ec6
KennethNielsen89 <kolob771@hotmail.com>
2017-02-23 12:00:30 +0100

    JS

---
+++ Petrograd.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("siden vises");
+
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+    visProdukt();
+}
+
+function visProduktListe( listen ) {
+    console.table( listen );
+    listen.forEach( visProdukt )
+}
+
+function visProdukt( produkt ) {
+    var klon = document.querySelector(".produkt_template").content.cloneNode(true);
+
+    document.querySelector(".produkt_liste").appendChild(klon);
+}
